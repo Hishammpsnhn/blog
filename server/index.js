0289ac3a9d6637c33330f5ebea18391cf1b356ac
hishammpsnhn <hishammpsn@gmail.com>
2025-03-16 13:51:39 +0530

    fix: cors issue

--- server/index.js
+++ server/index.js
@@ -11,7 +11,7 @@ dotenv.config();
 const app = express();
 const allowedOrigins = [
   process.env.CLIENT_URL, 
-  "http://localhost:5174",
+  "http://localhost:5173",
   "https://shiny-speculoos-15727b.netlify.app/" 
 ];
 
